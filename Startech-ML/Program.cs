b6bb35bbbfc2b1af74e410aa10942544a9af18ca
MorenaLopezObejero <obejeromorena@gmail.com>
2026-02-11 21:41:01 -0300

    Agregue Log a Program.cs de Consola

--- Startech-ML/Program.cs
+++ Startech-ML/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
-using StartechML.Api;
+﻿using StartechML.Api;
 using StartechML.Models;
 using StartechML.Services;
+using StartechML.Utils;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 
 namespace StartechML
@@ -13,55 +14,52 @@ namespace StartechML
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("StartechML - Crear publicaciones en Mercado Libre\n");
+            //1️ INICIALIZAR LOGGER
+            Logger.SetLogPath(Path.Combine(AppContext.BaseDirectory, "Log"));
+            Logger.SetLogLine(85);
+            Logger.Write("Inicio aplicación StartechML (Consola)", "Y", "Y", Logger.Mode.Info.ToString());
 
 
-            //TOKEN OAUTH VÁLIDO
-            string accessToken = "APP_USR-2804901742283043-021120-14066706e6fcbd181096f4d0a9ba1085-1170413717";
-
-
-            //INICIALIZAR CLIENTE Y SERVICIO
-
-            var mlClient = new MercadoLibreClient(accessToken);
-            var publicationService = new BulkPublicationService(mlClient);
-
-
-            //3️ CARGAR PUBLICACIONES DESDE JSON
-
-            Console.WriteLine("Cargando publicaciones desde archivo JSON...\n");
-
-            List<PublicationRequest> publications;
 
             try
             {
+                Console.WriteLine("StartechML - Crear publicaciones en Mercado Libre\n");
+                //TOKEN OAUTH VÁLIDO
+                string accessToken = "APP_USR-2804901742283043-021120-14066706e6fcbd181096f4d0a9ba1085-1170413717";
+
+                Logger.Write("Token cargado correctamente", "Y", "Y", Logger.Mode.Info.ToString());
+                //INICIALIZAR CLIENTE Y SERVICIO
+
+                var mlClient = new MercadoLibreClient(accessToken);
+                var publicationService = new BulkPublicationService(mlClient);
+                //3️ CARGAR PUBLICACIONES DESDE JSON
+
+                Console.WriteLine("Cargando publicaciones desde archivo JSON...\n");
+                Logger.Write("Cargando publicaciones desde JSON", "Y", "Y", Logger.Mode.Info.ToString());
+
                 var basePath = AppContext.BaseDirectory;
                 var jsonPath = Path.Combine(basePath, "data", "Publication.json");
 
-                publications = FilePublicationLoader.LoadFromJson(jsonPath);
+                List<PublicationRequest> publications = 
+                    FilePublicationLoader.LoadFromJson(jsonPath);
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("❌ Error al cargar el archivo de publicaciones:");
-                Console.WriteLine(ex.Message);
-                return;
-            }
+                Logger.Write($"Se cargaron {publications.Count} publicaciones", "Y", "Y", Logger.Mode.Info.ToString());
 
-            if (publications.Count == 0)
-            {
-                Console.WriteLine("⚠️ No hay publicaciones para procesar.");
-                return;
-            }
- 
-            //4️ EJECUTAR PUBLICACIÓN MASIVA
-            try
-            {
+                if (publications.Count == 0)
+                {
+                    Console.WriteLine("⚠️ No hay publicaciones para procesar.");
+                    Logger.Write("No hay publicaciones para procesar", "Y", "Y", Logger.Mode.Info.ToString());
+                    return;
+                }
+
+                //Ejecutar Publicacion masiva
                 await publicationService.PublishAllAsync(publications);
+
+                Logger.Write("Proceso finalizado correctamente", "Y", "Y", Logger.Mode.Info.ToString());
             }
             catch (Exception ex)
             {
-                Console.WriteLine("❌ Error general en el proceso:");
-                Console.WriteLine(ex.Message);
+                Logger.Write("Error general: " + ex.Message, "Y", "Y", Logger.Mode.Error.ToString());
             }
 
             Console.WriteLine("\nProceso finalizado");
