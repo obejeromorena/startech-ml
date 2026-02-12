c31b71b4e97dc98ef8080d42f1f69ecb3d851e62
MorenaLopezObejero <obejeromorena@gmail.com>
2026-02-11 22:04:33 -0300

    Agregue Log a FilePublicationLoader.cs FUNCIONA

--- Startech-ML/Services/FilePublicationLoader.cs
+++ Startech-ML/Services/FilePublicationLoader.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using StartechML.Models;
+using StartechML.Utils;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using StartechML.Models;
 
 namespace StartechML.Services
 {
@@ -11,9 +12,15 @@ namespace StartechML.Services
         // Carga una lista de publicaciones desde un archivo JSON
         public static List<PublicationRequest> LoadFromJson(string path)
         {
+            Logger.Write($"Intentando cargar archivo JSON: {path}", "Y", "Y", Logger.Mode.Info.ToString());
+
             // Verifica si el archivo existe
-            if (!File.Exists(path))
+            if (!File.Exists(path)) 
+            {
+                Logger.Write($"Archivo no encontrado: {path}", "Y", "Y", Logger.Mode.Error.ToString());
                 throw new FileNotFoundException($"No se encontró el archivo: {path}");
+            }
+
 
             // Lee todo el contenido del archivo
             var json = File.ReadAllText(path);
@@ -26,8 +33,16 @@ namespace StartechML.Services
             };
 
             // Convierte el JSON en una lista de PublicationRequest
-            return JsonSerializer.Deserialize<List<PublicationRequest>>(json, options)
-                   ?? new List<PublicationRequest>();
+            var publications = JsonSerializer.Deserialize<List<PublicationRequest>>(json, options)
+            ?? new List<PublicationRequest>();
+
+
+            Logger.Write($"Se cargaron {publications.Count} publicaciones desde JSON",
+                "Y",
+                "Y",
+                Logger.Mode.Info.ToString());
+
+            return publications;
         }
     }
 }
